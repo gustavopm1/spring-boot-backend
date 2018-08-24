6345d6f0986d99af04ef109de17c7a26b151908b
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-08-24 15:48:57 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/gpmoraes/desafio/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.gpmoraes.desafio.repositories;
+
+import com.gpmoraes.desafio.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/gpmoraes/desafio/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.gpmoraes.desafio.services;
+
+import com.gpmoraes.desafio.domain.Categoria;
+import com.gpmoraes.desafio.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
