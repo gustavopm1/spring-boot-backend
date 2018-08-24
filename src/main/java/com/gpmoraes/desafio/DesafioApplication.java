c8356644409a0f62f009649376e6d33423a4b272
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-08-24 15:59:28 -0300

    Criando operação de instanciação

--- src/main/java/com/gpmoraes/desafio/DesafioApplication.java
+++ src/main/java/com/gpmoraes/desafio/DesafioApplication.java
@@ -1,12 +1,31 @@
 package com.gpmoraes.desafio;
 
+import com.gpmoraes.desafio.domain.Categoria;
+import com.gpmoraes.desafio.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class DesafioApplication {
+public class DesafioApplication implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
 	}
 }
