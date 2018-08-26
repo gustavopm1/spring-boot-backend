beb87d524c0ec0bc048e2b043059413e72ee915d
Gustavo <gustavopmoraes@gmail.com>
2018-08-26 18:14:51 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/gpmoraes/desafio/services/CategoriaService.java
+++ src/main/java/com/gpmoraes/desafio/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.gpmoraes.desafio.services;
 
 import com.gpmoraes.desafio.domain.Categoria;
 import com.gpmoraes.desafio.repositories.CategoriaRepository;
+import com.gpmoraes.desafio.services.Exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,9 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
 
-    public Categoria buscar(Integer id){
+    public Categoria find(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
