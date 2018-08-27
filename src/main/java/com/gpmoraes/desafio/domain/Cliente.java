2cd973b967716ef65accee1c6de2f85e6af5165d
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-08-27 10:42:54 -0300

    Pedido, Pagamento e EstadoPagamento

--- src/main/java/com/gpmoraes/desafio/domain/Cliente.java
+++ src/main/java/com/gpmoraes/desafio/domain/Cliente.java
@@ -28,6 +28,9 @@ public class Cliente implements Serializable {
     @CollectionTable(name="TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
+
     public Cliente() {
     }
 
@@ -89,6 +92,14 @@ public class Cliente implements Serializable {
 
     public Set<String> getTelefones() {
         return telefones;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
     }
 
     public void setTelefones(Set<String> telefones) {
