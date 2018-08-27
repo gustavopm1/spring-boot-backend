fa10b6fe5cdc8708ed4648577ea27929397a8a0f
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-08-27 13:13:42 -0300

    Atualização: Utilização somente JsonIgnore

--- src/main/java/com/gpmoraes/desafio/domain/Pagamento.java
+++ src/main/java/com/gpmoraes/desafio/domain/Pagamento.java
@@ -1,6 +1,6 @@
 package com.gpmoraes.desafio.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gpmoraes.desafio.domain.enums.EstadoPagamento;
 
 import javax.persistence.*;
@@ -17,7 +17,7 @@ public abstract class Pagamento implements Serializable {
     private Integer id;
     private Integer estado;
 
-    @JsonBackReference
+    @JsonIgnore
     @OneToOne
     @JoinColumn(name = "pedido_id")
     @MapsId
