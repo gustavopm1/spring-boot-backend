bf0c1ab5d68df85260602cbefa6077655e57900f
Gustavo Pires de Moraes <gustavopmoraes@gmail.com>
2018-08-27 14:49:12 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/gpmoraes/desafio/services/CategoriaService.java
+++ src/main/java/com/gpmoraes/desafio/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.gpmoraes.desafio.services;
 
 import com.gpmoraes.desafio.domain.Categoria;
 import com.gpmoraes.desafio.repositories.CategoriaRepository;
+import com.gpmoraes.desafio.services.Exceptions.DataIntegrityException;
 import com.gpmoraes.desafio.services.Exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -28,5 +30,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj){
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id){
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
+        }
     }
 }
